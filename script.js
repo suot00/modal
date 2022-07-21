d5d1ab2be08374e6aaa14822c30058da3ebc5058
suot00 <truongkhacsuot661@gmail.com>
2022-07-21 17:51:44 +0700

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+var modal = document.querySelector(".modal");
+
+var btn = document.querySelector(".btn-modal");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
